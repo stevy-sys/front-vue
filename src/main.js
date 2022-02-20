fdf01833925ee5d9b4e9d2565c8d1bf1a5352206
stevy <stevyralambomanana@gmail.com>
2022-02-20 21:24:47 +0100

    plugin install

--- src/main.js
+++ src/main.js
@@ -3,19 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-// import { MdIcon,MdInput,MdCard,MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
-import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'vue-material/dist/vue-material.min.css'
 import vuetify from './plugins/vuetify'
-// import 'vue-material/dist/theme/default.css'
-
-
-// Vue.use(MdButton)
-// Vue.use(MdCard)
-// Vue.use(MdInput)
-// Vue.use(MdContent)
-// Vue.use(MdTabs)
-// Vue.use(MdIcon)
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 
