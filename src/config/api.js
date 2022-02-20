acb403ffd47a4b7736650af45cd0f6ef5d357668
stevy <stevyralambomanana@gmail.com>
2022-02-20 21:22:35 +0100

    config api

---
+++ src/config/api.js
@@ -0,0 +1,12 @@
+import { routeBasic } from "."
+
+export const routeAPi = {
+
+    //authentification
+    login:routeBasic + 'login' ,
+    register:routeBasic + 'register' ,
+
+
+    //user
+    myProfil:routeBasic + 'user'
+}
